ee8df22202835f1c2cbe5d6d8cf15146db06e4de
vatsal <vso.opash@gmail.com>
2024-04-30 12:09:24 +0530

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  hmr: false,
+  serverUrl: 'https://api.hindu.social/api/v1/',
+  serverUrl2: 'http://localhost:8080/api/v1/',
+  socketUrl: 'https://api.hindu.social/',
+  socketUrl2: 'http://localhost:8080/',
+  imgUrl: 'root/storage/uploads',
+
+};
