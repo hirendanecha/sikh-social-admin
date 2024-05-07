7c8a91e299465be71264bc0adcf2885c971bb402
vatsal <vso.opash@gmail.com>
2024-05-07 18:04:49 +0530

    Change APIs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   hmr: false,
-  serverUrl: 'https://api.hindu.social/api/v1/',
+  serverUrl: 'https://api.sikh.social/api/v1/',
   serverUrl2: 'http://localhost:8080/api/v1/',
-  socketUrl: 'https://api.hindu.social/',
+  socketUrl: 'https://api.sikh.social/',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
 
